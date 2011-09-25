410ce6fd30f4546b8348c04af24c9a8a9b6a2172
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-09-24 20:46:54 -0400

    Backgrounding support

--- Project1/dispatch.c
+++ Project1/dispatch.c
@@ -1,17 +1,24 @@
 #include "dispatch.h"
 
+#include <stdbool.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
 bool dispatch(char** tokens, const int tokensLength) {
-  if(strcmp(tokens[0], "exit") == 0) return false;
+  if(*tokens[0] == EOF || strcmp(tokens[0], "exit") == 0) return false;
+
+  bool runInBackground = false;
+  if(strcmp(tokens[tokensLength - 1], "&") == 0) {
+    tokens[tokensLength - 1] = NULL;
+  }
 
   int pid = fork();
 
   if(pid == 0) {
     execvp(tokens[0], tokens);
-  } else {
+  } else if(!runInBackground) {
     waitpid(pid, NULL, 0);
   }
 
