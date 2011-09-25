ad3ea2f2431e5adbb2851ca70ad7f1ee59b9f82a
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-09-25 03:35:19 -0400

    Actually background when asked

--- Project1/dispatch.c
+++ Project1/dispatch.c
@@ -10,15 +10,19 @@ bool dispatch(char** tokens, const int tokensLength) {
   if(tokens[0] == NULL) return true;
   if(*tokens[0] == EOF || strcmp(tokens[0], "exit") == 0) return false;
 
-  bool runInBackground = false;
+  bool runInBackground;
   if(strcmp(tokens[tokensLength - 1], "&") == 0) {
+    runInBackground = true;
     tokens[tokensLength - 1] = NULL;
+  } else {
+    runInBackground = false;
   }
 
   int pid = fork();
 
   if(pid == 0) {
     execvp(tokens[0], tokens);
+    if(runInBackground) return false;
   } else if(!runInBackground) {
     waitpid(pid, NULL, 0);
   }
