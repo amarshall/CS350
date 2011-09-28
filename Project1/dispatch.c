0b4a7e23f3677ca2e8669dd93d27e47e3363b83a
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-09-28 04:16:36 -0400

    Gratuitous error checking

--- Project1/dispatch.c
+++ Project1/dispatch.c
@@ -13,11 +13,13 @@ bool dispatch(char** tokens, const int tokensLength, bool runInBackground, char*
   if(*tokens[0] == EOF || strcmp(tokens[0], "exit") == 0) return false;
 
   int pid = fork();
+  if(pid == -1) { perror(NULL); return false; }
 
   if(pid == 0) {
     if(outputFile != NULL) {
       int output = open(outputFile, O_CREAT | O_WRONLY | O_TRUNC, 0644);
-      dup2(output, 1);
+      if(output == -1) { perror(NULL); return false; }
+      if(dup2(output, 1) == -1) { perror(NULL); return false; }
     }
 
     if(inputFile != NULL) {
@@ -25,17 +27,19 @@ bool dispatch(char** tokens, const int tokensLength, bool runInBackground, char*
       pipe(pipes);
 
       int pid2 = fork();
+      if(pid == -1) { perror(NULL); return false; }
       if(pid2 == 0) {
         close(pipes[1]);
-        dup2(pipes[0], 0);
-        execvp(tokens[0], tokens);
+        if(dup2(pipes[0], 0) == -1) { perror(NULL); return false; }
+        if(execvp(tokens[0], tokens) == -1) { perror("Failed to execute command"); return false; }
       } else {
         close(pipes[0]);
 
         FILE* input = fopen(inputFile, "r");
+        if(input == NULL) { perror(NULL); return false; }
         char buffer[64];
         while(fgets(buffer, 64, input) != NULL) {
-          write(pipes[1], buffer, strlen(buffer));
+          if(write(pipes[1], buffer, strlen(buffer)) == -1) { perror(NULL); return false; }
         }
         close(pipes[1]);
         fclose(input);
@@ -44,7 +48,7 @@ bool dispatch(char** tokens, const int tokensLength, bool runInBackground, char*
         return false;
       }
     } else {
-      execvp(tokens[0], tokens);
+      if(execvp(tokens[0], tokens) == -1) { perror("Failed to execute command"); return false; }
     }
   } else if(!runInBackground) {
     waitpid(pid, NULL, 0);
