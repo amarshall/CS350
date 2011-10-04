4ef6689c28aa90994eff89a1218dafabbe9c6bc6
Andrew Marshall <andrew@johnandrewmarshall.com>
2011-10-04 18:36:58 -0400

    remove excessive complexity for input redirection

--- Project1/dispatch.c
+++ Project1/dispatch.c
@@ -23,33 +23,12 @@ bool dispatch(char** tokens, const int tokensLength, bool runInBackground, char*
     }
 
     if(inputFile != NULL) {
-      int pipes[2];
-      pipe(pipes);
-
-      int pid2 = fork();
-      if(pid == -1) { perror(NULL); return false; }
-      if(pid2 == 0) {
-        close(pipes[1]);
-        if(dup2(pipes[0], 0) == -1) { perror(NULL); return false; }
-        if(execvp(tokens[0], tokens) == -1) { perror("Failed to execute command"); return false; }
-      } else {
-        close(pipes[0]);
-
-        FILE* input = fopen(inputFile, "r");
-        if(input == NULL) { perror(NULL); return false; }
-        char buffer[64];
-        while(fgets(buffer, 64, input) != NULL) {
-          if(write(pipes[1], buffer, strlen(buffer)) == -1) { perror(NULL); return false; }
-        }
-        close(pipes[1]);
-        fclose(input);
-
-        if(!runInBackground) waitpid(pid2, NULL, 0);
-        return false;
-      }
-    } else {
-      if(execvp(tokens[0], tokens) == -1) { perror("Failed to execute command"); return false; }
+      int input = open(inputFile, O_RDONLY, 0644);
+      if(input == -1) { perror(NULL); return false; }
+      if(dup2(input, 0) == -1) { perror(NULL); return false; }
     }
+
+    if(execvp(tokens[0], tokens) == -1) { perror("Failed to execute command"); return false; }
   } else if(!runInBackground) {
     waitpid(pid, NULL, 0);
   }
